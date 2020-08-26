aec2403da140d56a6cf7a890ab4d9e8e85aceeef
GlennBarosen <glennbarosen@gmail.com>
2020-08-26 16:11:26 +0200

    API working 🤠

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-console.log(process.env.REACT_APP_API_KEY);
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+const URL = 'http://www.omdbapi.com/?apikey=' + API_KEY + '&';
+
+interface Movie {
+  Poster: string
+  Title: string
+  Type: string
+  Year: string
+  imdbID: string
+}
+
+interface Result {
+  Search: []
+
+}
 
 const App = () => {
 
+  const [result, setResult] = useState({ Search: [] } as Result);
+
+  useEffect(() => {
+    const getResult = async () => {
+      const response = await fetch(URL + 's=harry potter');
+      const jsonResponse = await response.json();
+      setResult(jsonResponse);
+    }
+    getResult();
+  }, []);
+  console.log(result)
   return (
-    <h1>Hello Word!</h1>
 
-
+    <div>
+      <h1>Matrix</h1>
+      <div className='Container'>
+        <ul>
+          {
+            result.Search.map((movie: Movie) => {
+              return <li key={movie.imdbID}>{movie.Title}</li>
+            })
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
